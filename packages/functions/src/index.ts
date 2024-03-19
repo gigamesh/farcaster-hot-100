45d18769217861ced59343ee3156c83f137bed6a
Matt Masurka <m.masurka@gmail.com>
2024-03-19 12:04:12 -0700

    eslint & prettier fixes

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -1,17 +1,17 @@
 import * as functions from "firebase-functions";
 import fetch from "node-fetch";
-const {  defineString } = require('firebase-functions/params');
+import { defineString } from "firebase-functions/params";
 
-const CRON_SECRET = defineString('CRON_SECRET');
+const CRON_SECRET = defineString("CRON_SECRET");
 
-exports.bot = functions.pubsub
-.schedule('0 19 * * *')
-.onRun(async () => {
-  fetch('https://fc.hot100.xyz/api/bot', {headers: { 'Authorization': CRON_SECRET }})
+exports.bot = functions.pubsub.schedule("0 19 * * *").onRun(async () => {
+  fetch("https://fc.hot100.xyz/api/bot", {
+    headers: { Authorization: CRON_SECRET.value() },
+  });
 });
 
-exports.purge = functions.pubsub
-.schedule('0 */8 * * *')
-.onRun(async () => {
-  fetch('https://fc.hot100.xyz/api/fetch', {headers: { 'Authorization': CRON_SECRET }})
+exports.purge = functions.pubsub.schedule("0 */8 * * *").onRun(async () => {
+  fetch("https://fc.hot100.xyz/api/fetch", {
+    headers: { Authorization: CRON_SECRET.value() },
+  });
 });

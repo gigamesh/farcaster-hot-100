15cd53bd9f2a85ead7edab5d13ad97e2f6f849a9
Matt Masurka <m.masurka@gmail.com>
2024-03-19 11:43:15 -0700

    node-fetch

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from "firebase-functions";
+import fetch from "node-fetch";
 const {  defineString } = require('firebase-functions/params');
 
 const CRON_SECRET = defineString('CRON_SECRET');
@@ -10,7 +11,7 @@ exports.bot = functions.pubsub
 });
 
 exports.purge = functions.pubsub
-.schedule('0 18 * * *')
+.schedule('0 */8 * * *')
 .onRun(async () => {
   fetch('https://fc.hot100.xyz/api/fetch', {headers: { 'Authorization': CRON_SECRET }})
 });
